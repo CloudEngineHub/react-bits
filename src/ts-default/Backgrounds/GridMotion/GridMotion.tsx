85981fc0a38ef11aad10ec171d222d48e7cd9ea4
David Haz <davidhzdev@gmail.com>
2025-11-14 11:20:43 +0200

    format

--- src/ts-default/Backgrounds/GridMotion/GridMotion.tsx
+++ src/ts-default/Backgrounds/GridMotion/GridMotion.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, FC, ReactNode} from 'react';
+import { useEffect, useRef, FC, ReactNode } from 'react';
 import { gsap } from 'gsap';
 import './GridMotion.css';
 
